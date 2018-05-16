94f1980441e7e4e4e49c47c36bddf48fd74eaba9
wendy <fengwen@whtriples.com>
2018-05-16 12:07:48 +0800

    first commit

---
+++ component/picker/picker.js
@@ -0,0 +1,38 @@
+Vue.component('picker', {
+    template: '<div><transition name="fade"><div v-show="show" class="mask"></div></transition>\
+    <transition name="picker-slide-fade">\
+        <div v-show="show" class="picker-modal">\
+            <div class="picker-control">\
+                <span v-if="text.length == 0">请选择</span>\
+                <span v-else>{{text}}</span>\
+                <a v-on:click="sumbit">确定</a>\
+            </div>\
+            <div class="picker-content">\
+                <p v-for="obj in list" \
+                :data-value="obj.value" v-on:click="select">{{obj.text}}</p>\
+            </div>\
+        </div>\
+    </transition></div>',
+    data: function() {
+        return {
+            text: '',
+            value: ''
+        };
+    },
+    props: {
+        list: Array,
+        show: Boolean,
+    },
+    methods: {
+        select: function (e) {
+            var value = e.currentTarget.getAttribute('data-value');
+            var text = e.currentTarget.innerHTML;
+            this.text = text;
+            this.value = value;
+        },
+        sumbit: function () {
+            this.$emit('change', [this.text, this.value]);
+            this.$emit('close');
+        }
+    }
+});
---
+++ component/uploadpic/uploadpic.js
@@ -0,0 +1,35 @@
+Vue.component('upload-pic', {
+    template: '<div style="display: inline-block;"><div v-if="value.length == 0">\
+        <input class="input-hide" type="file" :id="name" v-on:change="addPic">\
+        <label :for="name" class="upload-pic"></label>\
+    </div>\
+    <div v-else>\
+        <div class="show-pic">\
+            <i class="ico-del" v-on:click="removePic"></i>\
+            <img class="img-cover" :src="value">\
+        </div>\
+    </div></div>',
+    data: function() {
+        return {
+            value: ''
+        };
+    },
+    props: {
+        name: String
+    },
+    methods: {
+        addPic: function (e) {
+            var _this = this;
+            // 单图上传
+            var file = e.currentTarget.files[0];
+            var reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = function(e){
+                _this.value = this.result;
+            }
+        },
+        removePic: function (e) {
+            this.value = '';
+        }
+    }
+});
